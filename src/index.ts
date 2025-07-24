d85565f751d8ee154b321c561a51c5a1adf917f6
Upwork-Job32 <renan.work32@gmail.com>
2025-07-24 20:59:42 +0800

    feat: make the webhook

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import { Request, Response } from "express";
+import bodyParser from "body-parser";
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/webhooks/customers/data_request', (req: Request, res: Response) => {
   // Log the request for compliance tracking
